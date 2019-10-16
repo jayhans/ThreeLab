e3ab34c5c14bf227f83e6bfedde12472a9ed82b9
hosuk <kirklayer@gmail.com>
2019-10-16 17:07:37 +0900

    raycast init

--- src/App.js
+++ src/App.js
@@ -8,10 +8,72 @@ import OrbitControls from "three-orbitcontrols";
 var camera, scene, renderer;
 var geometry, material, mesh;
 
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+function onMouseMove(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // update the picking ray with the camera and mouse position
+  raycaster.setFromCamera(mouse, camera);
+
+  // calculate objects intersecting the picking ray
+  //var intersects = raycaster.intersectObjects(scene.children);
+  //console.log(intersects)
+  
+  // 자식 및으로만 서칭 ... 
+  var intersects = raycaster.intersectObjects(scene.children, true);
+  //console.log(intersects)
+
+  if(intersects){
+     console.log(intersects[0])
+    // if(intersects[0].object.material.wireframe){
+    //   intersects[0].object.material.wireframe = true
+    // } 
+  }
+}
+
+function onMouseClick(event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  console.log(mouse)
+
+   // update the picking ray with the camera and mouse position
+   raycaster.setFromCamera(mouse, camera);
+
+   // calculate objects intersecting the picking ray
+   //var intersects = raycaster.intersectObjects(scene.children);
+   //console.log(intersects)
+
+   var intersects = raycaster.intersectObjects(scene.children, true);
+   console.log(intersects)
+
+   if(intersects){
+      console.log(intersects[0])
+     //intersects[0].object.material.wireframe = true
+   }
+
+  //  for (var i = 0; i < intersects.length; i++) {
+  //    intersects[i].object.material.wireframe = true
+  //  }
+
+}
+//'#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
 function App() {
   var threeRef = useRef(null);
 
   function init() {
+    window.addEventListener( 'click', onMouseClick, false );
+    window.addEventListener( 'mousemove', onMouseMove, false );
     camera = new THREE.PerspectiveCamera(
       70,
       window.innerWidth / window.innerHeight,
@@ -20,16 +82,25 @@ function App() {
     );
     camera.position.z = 10;
     // y z axis exchange
-    camera.up = new THREE.Vector3( 0, 0, 1)
+    camera.up = new THREE.Vector3(0, 0, 1);
 
     scene = new THREE.Scene();
+
+    var tg = new THREE.SphereGeometry(5,5,5)
+    var tm = new THREE.MeshNormalMaterial();
+    var tmesh = new THREE.Mesh(tg, tm);
 
     geometry = new THREE.BoxGeometry(2, 2, 2);
     material = new THREE.MeshNormalMaterial();
 
     mesh = new THREE.Mesh(geometry, material);
-    //mesh.position.set(10, 0, 0)
-    scene.add(mesh);
+
+    mesh.position.set(10, 0, 0)
+
+    tmesh.add(mesh)
+    scene.add(tmesh);
+
+
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -40,16 +111,14 @@ function App() {
     controls.enableZoom = true;
     threeRef.current.appendChild(renderer.domElement);
 
-    var GridHelper = new THREE.GridHelper(1000,100)
+    var GridHelper = new THREE.GridHelper(1000, 100);
     //  y z axis exchange
-    GridHelper.rotation.set(Math.PI/2 , 0 , 0)
-    scene.add(GridHelper)
+    GridHelper.rotation.set(Math.PI / 2, 0, 0);
+    scene.add(GridHelper);
 
-    var AxesHelper = new THREE.AxesHelper(50)
-    AxesHelper.position.set(0,0,0.1)
-    scene.add(AxesHelper)
-
-    //THREE.Def
+    var AxesHelper = new THREE.AxesHelper(50);
+    AxesHelper.position.set(0, 0, 0.1);
+    scene.add(AxesHelper);
   }
 
   function animate() {
@@ -58,7 +127,10 @@ function App() {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
+   
     renderer.render(scene, camera);
+
+    //renderer.render(scene, camera);
   }
 
   useEffect(() => {
