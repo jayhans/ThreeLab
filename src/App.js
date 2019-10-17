18cb0e014533256e044f58c2e10ef7cb293a4d6f
hosuk <kirklayer@gmail.com>
2019-10-17 13:46:32 +0900

    transtest

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "./App.css";
 import * as THREE from "three";
 import OrbitControls from "three-orbitcontrols";
 
-import Stats from 'stats.js'
+import Stats from "stats.js";
 
 var camera, scene, renderer;
 var geometry, material, mesh;
 
 var stats = new Stats();
-stats.showPanel( 0 ); // 0:fps, 1:ms, 2: mb, 3+: custom
+stats.showPanel(0); // 0:fps, 1:ms, 2: mb, 3+: custom
+
+var texture = new THREE.TextureLoader().load("brick_diffuse.jpg")
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 9, 1 );
 
 function App() {
   var threeRef = useRef(null);
@@ -23,15 +28,15 @@ function App() {
       0.01,
       1000
     );
-    camera.position.z = 10;
+    camera.position.z = 400;
     // y z axis exchange
-    camera.up = new THREE.Vector3( 0, 0, 1)
+    camera.up = new THREE.Vector3(0, 0, 1);
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(2, 2, 2);
-    var material = new THREE.MeshBasicMaterial( { color: 0x2194CE } );
-
+    geometry = new THREE.BoxGeometry(200, 200, 200);
+    var material = new THREE.MeshLambertMaterial({  map: texture, transparent:true, opacity:0.5});
+    
     mesh = new THREE.Mesh(geometry, material);
     //mesh.position.set(10, 0, 0)
     scene.add(mesh);
@@ -44,30 +49,39 @@ function App() {
     controls.dampingFactor = 0.25;
     controls.enableZoom = true;
     threeRef.current.appendChild(renderer.domElement);
-    threeRef.current.appendChild(stats.dom)
+    threeRef.current.appendChild(stats.dom);
 
-    var GridHelper = new THREE.GridHelper(1000,100)
+    var GridHelper = new THREE.GridHelper(1000, 100);
     //  y z axis exchange
-    GridHelper.rotation.set(Math.PI/2 , 0 , 0)
-    scene.add(GridHelper)
+    GridHelper.rotation.set(Math.PI / 2, 0, 0);
+    scene.add(GridHelper);
 
-    var AxesHelper = new THREE.AxesHelper(50)
-    AxesHelper.position.set(0,0,0.1)
-    scene.add(AxesHelper)
+    var AxesHelper = new THREE.AxesHelper(50);
+    AxesHelper.position.set(0, 0, 0.1);
+    scene.add(AxesHelper);
 
-    var light = new THREE.AmbientLight( 0x000000 );
-scene.add( light );
+    var light = new THREE.AmbientLight(0x000000);
+    scene.add(light);
 
-var lights = [];
-			lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-			lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-			lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-			lights[ 0 ].position.set( 0, 200, 0 );
-			lights[ 1 ].position.set( 100, 200, 100 );
-			lights[ 2 ].position.set( - 100, - 200, - 100 );
-			scene.add( lights[ 0 ] );
-			scene.add( lights[ 1 ] );
-			scene.add( lights[ 2 ] );
+    var lights = [];
+    lights[0] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[3] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[4] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[5] = new THREE.PointLight(0xffffff, 1, 0);
+    lights[0].position.set(0, 200, 0);
+    lights[1].position.set(100, 200, 100);
+    lights[2].position.set(-100, -200, -100);
+    lights[3].position.set(0, -200, 0);
+    lights[4].position.set(0,0,200);
+    lights[5].position.set(200,0,0);
+    scene.add(lights[0]);
+    scene.add(lights[1]);
+    scene.add(lights[2]);
+    scene.add(lights[3]);
+    scene.add(lights[4]);
+    scene.add(lights[5]);
 
     //THREE.Def
   }
